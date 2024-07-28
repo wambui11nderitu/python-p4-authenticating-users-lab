cd35528d726f8a3c91ad744162b898de96c529fb
wambui11nderitu <wambuinderitu93@gmail.com>
2024-07-29 01:55:52 +0300

    updating labs

--- server/app.py
+++ server/app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -16,42 +14,28 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-api = Api(app)
+@app.route('/clear')
+def clear_session():
+    session['page_views'] = 0
+    return {'message': '200: Successfully cleared session data'}, 200
 
-class ClearSession(Resource):
+@app.route('/articles')
+def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-    def delete(self):
+@app.route('/articles/<int:id>')
+def show_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        if article:
+            return make_response(jsonify(article.to_dict()), 200)
+        else:
+            return {'message': 'Article not found'}, 404
+        
+    return {'message': 'Maximum pageview limit reached'}, 401
     
-        session['page_views'] = None
-        session['user_id'] = None
-
-        return {}, 204
-
-class IndexArticle(Resource):
-    
-    def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
-
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
-
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
